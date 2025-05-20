887bb3bfbbbd0e05f0504434d4f1862e77c354eb
Silence2361 <mamedasadulaev@gmail.com>
2025-05-20 17:17:54 +0300

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/appeals-db';
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || '';
 
 export const connectDB = async () => {
     try {
